7128fbaaa2bfc3441187cd0d83cbf4fe05dc24a4
innovata <innovata@users.noreply.github.com>
2024-12-23 22:04:22 +0900

    0.2.5

---
+++ apps/client01.py
@@ -0,0 +1,38 @@
+
+
+
+import pprint 
+pp = pprint.PrettyPrinter(indent=2)
+import os 
+from importlib import reload
+
+
+from pytubefix import YouTube 
+from pytubefix.cli import on_progress
+
+
+import sys 
+sys.path.append("D:\pypjts\iCrawler\src")
+
+# from ipycrawl.crawlers.youtube import pytubefix 
+# reload(pytubefix)
+
+url = input("https://youtu.be/Jrmb07ku6ZY?feature=shared")
+
+print('URL: %s' % url)
+
+yt = YouTube(
+    url, 
+    use_oauth=True, 
+    allow_oauth_cache=True, 
+    on_progress_callback=on_progress
+)
+print(yt)
+print(yt.title)
+ys = yt.streams.get_highest_resolution()
+print(ys)
+ys.download()
+
+for st in yt.streams:
+    print(st)
+print(yt.fmt_streams)
