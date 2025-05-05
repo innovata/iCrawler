12d2e64c41432d2a6fee778f02697451cfe11156
innovata <iinnovata@gmail.com>
2025-05-06 07:42:29 +0900

    0.3.17

--- src/ipycrawl/seleniumX.py
+++ src/ipycrawl/seleniumX.py
@@ -6,25 +6,35 @@ Selenium-eXtension
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import pprint 
+pp = pprint.PrettyPrinter(indent=2)
 
 
 from ipylib.idebug import *
 
 
+import logging
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 
 def get_driver():
     options = webdriver.EdgeOptions()
     options.add_argument("--start-maximized")
+    print("\nOptions -->")
     pp.pprint(options.__dict__)
+
     driver = webdriver.Edge(options=options)
+    print("\nDriver -->")
     pp.pprint(driver.__dict__)
     return driver
+
 
 """엔터키 누르기"""
 def press_enter(driver):
